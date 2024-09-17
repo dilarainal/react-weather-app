a4fdd6c2c7f1d2dbacebae78151af834df3a7db4
dilarainal <148790405+dilarainal@users.noreply.github.com>
2024-09-16 21:23:10 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by {""}
-        <a
-          href="https://www.linkedin.com/in/dilarainal/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Dilara Inal
-        </a>
-        {""} and open-sourced on {""}
-        <a
-          href="https://github.com/dilarainal/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by {""}
+          <a
+            href="https://www.linkedin.com/in/dilarainal/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Dilara Inal
+          </a>
+          {""} and is open-sourced on {""}
+          <a
+            href="https://github.com/dilarainal/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
