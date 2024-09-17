75f37c48832d2013ce06e482585b0b450e1b1b17
dilarainal <148790405+dilarainal@users.noreply.github.com>
2024-09-16 21:06:28 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by {""}
+        <a
+          href="https://www.linkedin.com/in/dilarainal/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Dilara Inal
+        </a>
+        {""} and open-sourced on {""}
+        <a
+          href="https://github.com/dilarainal/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
