99127cadd5d8c46e0925bb8a11e2b50901524655
dilarainal <148790405+dilarainal@users.noreply.github.com>
2024-09-16 22:07:23 -0400

    Created basic outline for weather app

---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 7:00</li>
+        <li>Sunny</li>
+      </ul>
+
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="Sunny"
+          />
+          6° F | C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 5%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 3 mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
