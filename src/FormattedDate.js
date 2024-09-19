6d2e4fc24a0e07368d2e0f1f97942810039142d7
dilarainal <148790405+dilarainal@users.noreply.github.com>
2024-09-19 14:51:39 -0400

    Created a new component for the date

---
+++ src/FormattedDate.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function FormattedDate(props) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[props.date.getDay()];
+  let hour = props.date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minute = props.date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+
+  return (
+    <div>
+      {day} {hour}:{minute}
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -9,7 +10,7 @@ export default function Weather(props) {
     setWeatherData({
       ready: true,
       city: response.data.city,
-      date: "Wednesday 7:00",
+      date: new Date(response.data.time * 1000),
       description: response.data.condition.description,
       icon: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
       temperature: response.data.temperature.current,
@@ -48,7 +49,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
 
