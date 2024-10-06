adfb104a56cd84ab758b817c7d4d49331fdac2d7
dilarainal <148790405+dilarainal@users.noreply.github.com>
2024-10-06 14:19:23 -0400

    Created function to display 5 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -13,13 +13,18 @@ export default function WeatherForecast(props) {
   }
 
   if (loaded) {
-    console.log(forecastDay);
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecastDay[0]} />
-          </div>
+          {forecastDay.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
