35c444a321d84f783d7b6b95be48ac1fee22cbd9
dilarainal <148790405+dilarainal@users.noreply.github.com>
2024-10-06 15:04:27 -0400

    Added useEffect so content updates properly when searching
    useEffect solved the issue where the forecast did not update when a new city was searched.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
@@ -6,6 +6,10 @@ import axios from "axios";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecastDay, setForecastDay] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecastDay(response.data.daily);
